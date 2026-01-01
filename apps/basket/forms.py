464e1a91a9079bb5b2c96935e840718b58c73b10
remgo <37494165+dvladimirdm@users.noreply.github.com>
2026-01-01 04:39:17 -0500

    feat: enhance AddToBasketForm to support image uploads and update form template

--- apps/basket/forms.py
+++ apps/basket/forms.py
@@ -1,22 +1,14 @@
+# apps/basket/forms.py
 import os
 from django import forms
-from django.conf import settings
 from django.core.files.storage import default_storage
 from oscar.apps.basket.forms import AddToBasketForm as OriginalAddToBasketForm
+from oscar.apps.basket.forms import SimpleAddToBasketMixin #
 
 class AddToBasketForm(OriginalAddToBasketForm):
-    
-    # SOBRESCRIBIMOS ESTE MÉTODO ESPECÍFICO (Línea 232 de tu archivo original)
     def _add_option_field(self, product, option):
-        """
-        Dejamos que Oscar cree el campo normalmente, y si es el de la foto,
-        lo reemplazamos por un campo de archivo.
-        """
-        # 1. Ejecutar lógica original (crea el campo de texto)
         super()._add_option_field(product, option)
-        
-        # 2. Intervenir si es nuestra opción
-        # REEMPLAZA 'sube-tu-foto' con el código que viste en el paso 1
+        # Verifica el código exacto que obtuviste (ej: 'sube-tu-foto')
         if option.code == 'sube-tu-foto': 
             self.fields[option.code] = forms.ImageField(
                 label=option.name,
@@ -24,26 +16,21 @@ class AddToBasketForm(OriginalAddToBasketForm):
                 widget=forms.ClearableFileInput(attrs={'accept': 'image/*'})
             )
 
-    # Mantenemos la lógica de limpieza para guardar el archivo
     def clean(self):
         cleaned_data = super().clean()
-        
-        # REEMPLAZA AQUÍ TAMBIÉN EL CÓDIGO
         codigo_opcion = 'sube-tu-foto'
         
         if codigo_opcion in cleaned_data:
             imagen = cleaned_data.get(codigo_opcion)
             
-            # Si es un archivo real (tiene nombre y contenido)
+            # Si el campo es obligatorio y 'imagen' es una cadena (o está vacío) 
+            # en lugar de un objeto de archivo, Django fallará.
             if imagen and hasattr(imagen, 'name'):
-                # Definir ruta: media/uploads/2025/...
                 filename = imagen.name
                 ruta_relativa = os.path.join('uploads', filename)
-                
-                # Guardar físico
                 path = default_storage.save(ruta_relativa, imagen)
-                
-                # Reemplazar el objeto archivo por la ruta (string) para la BD
                 cleaned_data[codigo_opcion] = path
-                
         return cleaned_data
+
+class SimpleAddToBasketForm(SimpleAddToBasketMixin, AddToBasketForm):
+    pass
