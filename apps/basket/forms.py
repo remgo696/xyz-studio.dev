1ce3ea62e836b940e75b034e4df252576687a916
remgo <37494165+dvladimirdm@users.noreply.github.com>
2026-01-02 13:35:06 -0500

    feat: enhance form handling with custom styles and new layout for options

--- apps/basket/forms.py
+++ apps/basket/forms.py
@@ -1,44 +1,21 @@
-import os
 from django import forms
-from django.core.files.storage import default_storage
 from oscar.apps.basket.forms import AddToBasketForm as OriginalAddToBasketForm
-from oscar.apps.basket.forms import SimpleAddToBasketMixin
 
 class AddToBasketForm(OriginalAddToBasketForm):
     
     def _add_option_field(self, product, option):
         super()._add_option_field(product, option)
         
-        # 1. LÓGICA PARA SUBIR FOTO
+        # 1. Configuración para Subida de Fotos
         if option.code == 'sube-tu-foto': 
-            self.fields[option.code] = forms.ImageField(
-                label=option.name,
-                required=option.required,
-                widget=forms.ClearableFileInput(attrs={
-                    'accept': 'image/*',
-                    'class': 'file-dropzone'  # <--- INYECCIÓN DE CLASE CSS
-                })
-            )
-
-        # 2. LÓGICA PARA COLORES (Swatches)
-        # Detectamos si es color por el código o nombre
-        elif 'color' in option.code.lower() or 'colour' in option.code.lower():
-            self.fields[option.code].widget = forms.RadioSelect(attrs={
-                'class': 'color-swatch-list' # <--- INYECCIÓN DE CLASE CSS
+            self.fields[option.code].widget.attrs.update({
+                'class': 'file-dropzone',
+                'accept': 'image/*'
             })
-            
-    def clean(self):
-        cleaned_data = super().clean()
-        codigo_opcion = 'sube-tu-foto'
-        
-        if codigo_opcion in cleaned_data:
-            imagen = cleaned_data.get(codigo_opcion)
-            if imagen and hasattr(imagen, 'name'):
-                filename = imagen.name
-                ruta_relativa = os.path.join('uploads', filename)
-                path = default_storage.save(ruta_relativa, imagen)
-                cleaned_data[codigo_opcion] = path
-        return cleaned_data
 
-class SimpleAddToBasketForm(SimpleAddToBasketMixin, AddToBasketForm):
-    pass
+        # 2. Configuración para Colores (Radio Buttons)
+        elif 'color' in option.code.lower():
+            # Cambiamos el widget por defecto (Select) a RadioSelect
+            self.fields[option.code].widget = forms.RadioSelect(attrs={
+                'class': 'color-swatch-list'
+            })
