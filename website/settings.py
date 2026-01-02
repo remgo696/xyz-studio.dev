b2c8e479fec1ea6def5457001a94d03efc8c0931
remgo <37494165+dvladimirdm@users.noreply.github.com>
2026-01-01 20:45:34 -0500

    feat: create layout template and update styles for Tailwind CSS integration

--- website/settings.py
+++ website/settings.py
@@ -181,7 +181,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme/static')]
 
 # Media files (User-uploaded content)
 MEDIA_URL = '/media/'
